cf1a1ce8a54811a6a79a53dd960741d5f93b905d
Matthew Wiemer <mwiemer@outlook.com>
2015-03-02 12:00:29 -0600

    seems-to-be-working mmap

--- P3a/main.c
+++ P3a/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mymem.h"
 
 int main(int argc, char* argv[])
 {
 	printf("Hello World!\n");
+	void* addr = Mem_Init(15, 8);	
+	printf("Addr: %p\n", addr);
 	exit(0);
 }
--- P3a/mem.c
+++ P3a/mem.c
@@ -1,1 +1,44 @@
-// Malloc, Free, and Dump
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <assert.h>
+#include "else.h"
+
+extern bool initialized;
+
+void* Mem_Init(int sizeOfRegion, int slabSize)
+{
+	void* addr = NULL; // Starting address of newly mapped mem
+
+	if(!initialized) // Only do this stuff once
+	{
+		specialSize = slabSize;
+		//const char* map = "procMap";
+
+		//int newFile = open("procMap", O_RDWR); // Open file for mapping
+		//assert(newFile != -1); // Bail on error
+
+		addr = mmap(NULL, sizeOfRegion, PROT_READ | PROT_WRITE, 
+			MAP_ANON | MAP_PRIVATE, -1, 0); // Private to proc 
+			
+		assert(addr != MAP_FAILED); // Bail on error (for now?)	
+	}
+	
+	return addr; // Null pointer when initialized already!
+}
+
+void* Mem_Alloc(int size)
+{
+	return 0; // Null pointer!
+}
+
+int Mem_Free(void *ptr)
+{
+	return 0;
+}
+
+void Mem_Dump()
+{
+	return;
+}
