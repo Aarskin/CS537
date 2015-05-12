08e03e3a927298edc8eb1caca254180a5a466ee9
Matthew Wiemer <mwiemer@outlook.com>
2015-05-11 19:39:18 -0500

    started other half

---
+++ P5/lfscat.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "lfs.h"
+
+#define BSIZE 4096
+
+int main(int argc, char* argv[])
+{
+	if(arc < 2)
+	{
+		fprintf(stderr, "Usage: lfsreader <cat, ls> <abs path> <img file>");
+		exit(0);
+	}
+		
+	
+	exit(0);
+}
